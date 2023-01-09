9c8040d30342bff68ca8e4a386f866997e986d63
nicolas conde <ncondes@unal.edu.co>
2023-01-09 11:44:32 -0500

    createMatch test and function

--- src/library/useScoreboard.tsx
+++ src/library/useScoreboard.tsx
@@ -4,8 +4,23 @@ import { IMatch } from '../interfaces';
 export const useScoreboard = () => {
    const [scoreboard, setScoreboard] = useState<IMatch[]>([]);
 
-   const createMatch = () => {};
+   const createMatch = (homeTeam: string, awayTeam: string) => {
+      if (!homeTeam || !awayTeam)
+         throw new Error('team names were not provided');
+
+      const match: IMatch = {
+         awayTeam,
+         dateCreated: Date.now(),
+         homeTeam,
+         id: crypto.randomUUID().split('-')[0],
+         score: [0, 0],
+      };
+
+      setScoreboard((curr) => [...curr, match]);
+   };
+
    const finishMatch = () => {};
+
    const editMatch = () => {};
 
    const summary: IMatch[] = [];
--- test/library/useScoreboard.test.tsx
+++ test/library/useScoreboard.test.tsx
@@ -1,12 +1,9 @@
 import { describe, expect, it } from 'vitest';
 import { useScoreboard } from '../../src/library';
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 
 describe('useScoreboard', () => {
-   it('should be a function', () => {
-      expect(useScoreboard).toBeTypeOf('function');
-   });
-
+   // ## useScoreboard ##
    it('should return an object with the request operations and data', () => {
       const { result } = renderHook(() => useScoreboard());
       const { scoreboard, createMatch, finishMatch, editMatch, summary } =
@@ -17,5 +14,78 @@ describe('useScoreboard', () => {
       expect(finishMatch).toEqual(expect.any(Function));
       expect(editMatch).toEqual(expect.any(Function));
       expect(summary).toEqual(expect.any(Array));
+   });
+
+   // ## createMatch ##
+   it('should increment the length of the scoreboard by 1 upon creating a match', () => {
+      const { result } = renderHook(() => useScoreboard());
+      const { createMatch } = result.current;
+
+      const initialLength = result.current.scoreboard.length;
+
+      act(() => {
+         createMatch('Colombia', 'Peru');
+      });
+
+      const newLength = result.current.scoreboard.length;
+
+      expect(initialLength + 1).toBe(newLength);
+   });
+
+   it('shosuld add a new match with the names provided in the function as parameters', () => {
+      const { result } = renderHook(() => useScoreboard());
+      const { createMatch } = result.current;
+
+      act(() => {
+         createMatch('Colombia', 'Peru');
+      });
+
+      const { homeTeam, awayTeam } = result.current.scoreboard[0];
+
+      expect(homeTeam).toBe('Colombia');
+      expect(awayTeam).toBe('Peru');
+   });
+
+   it('should add a new match with a default score of [0,0] upon creating a match', () => {
+      const { result } = renderHook(() => useScoreboard());
+      const { createMatch } = result.current;
+
+      act(() => {
+         createMatch('Colombia', 'Peru');
+      });
+
+      const { score } = result.current.scoreboard[0];
+      expect(score).toEqual([0, 0]);
+   });
+
+   it('should have an id property', () => {
+      const { result } = renderHook(() => useScoreboard());
+      const { createMatch } = result.current;
+
+      act(() => {
+         createMatch('Colombia', 'Peru');
+      });
+
+      const { id } = result.current.scoreboard[0];
+      expect(Boolean(id)).toBe(true);
+   });
+
+   it('if there is match already stored in the scoreboard and we create a new one, length should increase again', () => {
+      const { result } = renderHook(() => useScoreboard());
+      const { createMatch } = result.current;
+
+      act(() => {
+         createMatch('Colombia', 'Peru');
+      });
+
+      const lengthAfterAddOneMatch = result.current.scoreboard.length;
+
+      act(() => {
+         createMatch('Venezuela', 'Bolivia');
+      });
+
+      const lengthAfterAddAnotherMatch = result.current.scoreboard.length;
+
+      expect(lengthAfterAddOneMatch + 1).toBe(lengthAfterAddAnotherMatch);
    });
 });
